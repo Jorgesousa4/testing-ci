69bbd23efa12831d36a5601cf33f0d7b8b20a2b9
Jorgesousa4 <jorgesousa074@gmail.com>
2025-01-06 21:19:52 -0300

    agora vai 1

--- primeiro_teste.robot
+++ primeiro_teste.robot
@@ -4,8 +4,9 @@ Library    SeleniumLibrary
 *** Variables ***
 ${URL}     https://google.com.br
 ${BROWSER}    Chrome
+${OPTIONS}    --headless    --disable-gpu    --no-sandbox    --disable-dev-shm-usage
 
 *** Test Cases ***
 Abrir Página Exemplo
-    Open Browser    ${URL}    ${BROWSER}
+    Open Browser    ${URL}    ${BROWSER}    options=${OPTIONS}
     Close Browser
--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
-from SeleniumLibrary import SeleniumLibrary
+from selenium.webdriver.chrome.options import Options
 
-class CustomSeleniumLibrary(SeleniumLibrary):
-    def __init__(self, *args, **kwargs):
-        driver_path = ChromeDriverManager().install()
-        service = Service(driver_path)
-        webdriver.Chrome(service=service)
-        super().__init__(*args, **kwargs)
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+
+driver = webdriver.Chrome(options=chrome_options)
